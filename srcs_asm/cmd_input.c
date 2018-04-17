8eb5fa19581ad562d93fc32f861c2407d407d243
cepalle <epalle.cyrille@gmail.com>
2018-04-17 19:06:29 +0200

    cmd_input start

--- srcs_asm/cmd_input.c
+++ srcs_asm/cmd_input.c
@@ -1,7 +1,33 @@
 #include "../includes/asm.h"
-
+#include <fcntl.h>
 
 t_cmdl cmd_input(int argc, char **argv)
 {
+	t_cmdl cmdl;
+	int fd;
 
+	cmdl = {0, 0, 0};
+	if (argc != 1 && argc != 0)
+	{
+		asm_usage();
+		cmdl.er = 1;
+		return cmdl;
+	}
+	if (argv == 1)
+	{
+		if (!ft_strequ("-a", argv[0]))
+		{
+			asm_usage();
+			cmdl.er = 1;
+			return cmdl;
+		}
+		cmdl.opt_a = 1;
+	}
+	fd = open(argv[0 + cmdl.opt_a], O_RDONLY);
+	if (fd < 0)
+	{
+		// perror
+		cmdl.er = 1;
+	}
+	return cmdl;
 };
