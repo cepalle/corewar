495006eb5c94709e2dbde164e55d1bd9f66d21e9
Epalle <cyrille.epalle@epita.fr>
2018-04-21 11:57:36 +0200

    check_insts reduce code

--- srcs_asm/check_semantics.c
+++ srcs_asm/check_semantics.c
@@ -74,249 +74,50 @@ int check_labels(t_parser parser_res, t_ast_inst *ast_inst)
 	return check_labels(parser_res, ast_inst->next);
 };
 
-int check_live(t_ast_inst *inst)
+int check_param(int ennum_token, int t_arg)
 {
-	if (inst->nb_ast_params != 1 ||
-			(inst->ast_params[0].enum_token != TOKEN_DIRECT_LABEL &&
-			 inst->ast_params[0].enum_token != TOKEN_DIRECT_NUMBER))
-	{
-		ft_printf("live bad parameters\n");
-		return 1;
-	}
-	return 0;
+    if ((t_arg & T_DIR) &&
+        (ennum_token == TOKEN_DIRECT_LABEL ||
+         ennum_token == TOKEN_DIRECT_NUMBER))
+        return 0;
+    if ((t_arg & T_IND) &&
+        (ennum_token == TOKEN_INDIRECT_LABEL ||
+         ennum_token == TOKEN_INDIRECT_NUMBER))
+        return 0;
+    if ((t_arg & T_REG) &&
+            ennum_token == TOKEN_LABEL)
+        return 0;
+    return 1;
 };
 
-int check_ld(t_ast_inst *inst)
-{
-	if (inst->nb_ast_params != 2 ||
-			(inst->ast_params[0].enum_token != TOKEN_DIRECT_LABEL &&
-			 inst->ast_params[0].enum_token != TOKEN_DIRECT_NUMBER &&
-			 inst->ast_params[0].enum_token != TOKEN_INDIRECT_LABEL &&
-			 inst->ast_params[0].enum_token != TOKEN_INDIRECT_NUMBER) ||
-			inst->ast_params[1].enum_token != TOKEN_LABEL)
-	{
-		ft_printf("ld bad parameters\n");
-		return 1;
-	}
-	return 0;
-};
+int check_cmd(t_ast_inst *inst, t_op op_desc) {
+    int i;
 
-int check_st(t_ast_inst *inst)
-{
-	if (inst->nb_ast_params != 2 ||
-			inst->ast_params[0].enum_token != TOKEN_LABEL ||
-			(inst->ast_params[1].enum_token != TOKEN_LABEL &&
-			 inst->ast_params[1].enum_token != TOKEN_INDIRECT_LABEL &&
-			 inst->ast_params[1].enum_token != TOKEN_INDIRECT_NUMBER))
-	{
-		ft_printf("st bad parameters\n");
-		return 1;
-	}
-	return 0;
-};
-
-int check_add(t_ast_inst *inst)
-{
-	if (inst->nb_ast_params != 3 ||
-			inst->ast_params[0].enum_token != TOKEN_LABEL ||
-			inst->ast_params[1].enum_token != TOKEN_LABEL ||
-			inst->ast_params[2].enum_token != TOKEN_LABEL)
-	{
-		ft_printf("add bad parameters\n");
-		return 1;
-	}
-	return 0;
-};
-
-int check_sub(t_ast_inst *inst)
-{
-	if (inst->nb_ast_params != 3 ||
-			inst->ast_params[0].enum_token != TOKEN_LABEL ||
-			inst->ast_params[1].enum_token != TOKEN_LABEL ||
-			inst->ast_params[2].enum_token != TOKEN_LABEL)
-	{
-		ft_printf("sub bad parameters\n");
-		return 1;
-	}
-	return 0;
-};
-
-int check_and(t_ast_inst *inst)
-{
-	if (inst->nb_ast_params != 3 ||
-			inst->ast_params[2].enum_token != TOKEN_LABEL)
-	{
-		ft_printf("and bad parameters\n");
-		return 1;
-	}
-	return 0;
-};
-
-int check_or(t_ast_inst *inst)
-{
-	if (inst->nb_ast_params != 3 ||
-			inst->ast_params[2].enum_token != TOKEN_LABEL)
-	{
-		ft_printf("or bad parameters\n");
-		return 1;
-	}
-	return 0;
-};
-
-int check_xor(t_ast_inst *inst)
-{
-	if (inst->nb_ast_params != 3 ||
-			inst->ast_params[2].enum_token != TOKEN_LABEL)
-	{
-		ft_printf("xor bad parameters\n");
-		return 1;
-	}
-	return 0;
-};
-
-int check_zjmp(t_ast_inst *inst)
-{
-	if (inst->nb_ast_params != 1 ||
-			(inst->ast_params[0].enum_token != TOKEN_DIRECT_NUMBER &&
-			 inst->ast_params[0].enum_token != TOKEN_DIRECT_LABEL))
-	{
-		ft_printf("zjmp bad parameters\n");
-		return 1;
-	}
-	return 0;
-};
-
-int check_ldi(t_ast_inst *inst)
-{
-	if (inst->nb_ast_params != 3 ||
-			(inst->ast_params[1].enum_token != TOKEN_DIRECT_NUMBER &&
-			 inst->ast_params[1].enum_token != TOKEN_DIRECT_LABEL &&
-			 inst->ast_params[1].enum_token != TOKEN_LABEL) ||
-			inst->ast_params[2].enum_token != TOKEN_LABEL)
-	{
-		ft_printf("ldi bad parameters\n");
-		return 1;
-	}
-	return 0;
-};
-
-int check_sti(t_ast_inst *inst)
-{
-	if (inst->nb_ast_params != 3 ||
-			inst->ast_params[0].enum_token != TOKEN_LABEL ||
-			(inst->ast_params[2].enum_token != TOKEN_DIRECT_NUMBER &&
-			 inst->ast_params[2].enum_token != TOKEN_DIRECT_LABEL &&
-			 inst->ast_params[2].enum_token != TOKEN_LABEL))
-	{
-		ft_printf("sti bad parameters\n");
-		return 1;
-	}
-	return 0;
-};
-
-int check_fork(t_ast_inst *inst)
-{
-	if (inst->nb_ast_params != 1 ||
-			(inst->ast_params[0].enum_token != TOKEN_DIRECT_NUMBER &&
-			 inst->ast_params[0].enum_token != TOKEN_DIRECT_LABEL))
-	{
-		ft_printf("fork bad parameters\n");
-		return 1;
-	}
-	return 0;
-};
-
-int check_lld(t_ast_inst *inst)
-{
-	if (inst->nb_ast_params != 2 ||
-			(inst->ast_params[0].enum_token != TOKEN_DIRECT_NUMBER &&
-			 inst->ast_params[0].enum_token != TOKEN_DIRECT_LABEL &&
-			 inst->ast_params[0].enum_token != TOKEN_INDIRECT_LABEL &&
-			 inst->ast_params[0].enum_token != TOKEN_INDIRECT_NUMBER) ||
-			inst->ast_params[1].enum_token != TOKEN_LABEL)
-	{
-		ft_printf("lld bad parameters\n");
-		return 1;
-	}
-	return 0;
-};
-
-int check_lldi(t_ast_inst *inst)
-{
-	if (inst->nb_ast_params != 3 ||
-			(inst->ast_params[1].enum_token != TOKEN_LABEL &&
-			 inst->ast_params[1].enum_token != TOKEN_DIRECT_LABEL &&
-			 inst->ast_params[1].enum_token != TOKEN_DIRECT_NUMBER) ||
-			inst->ast_params[2].enum_token != TOKEN_LABEL)
-	{
-		ft_printf("lldi bad parameters\n");
-		return 1;
-	}
-	return 0;
-};
-
-int check_lfork(t_ast_inst *inst)
-{
-	if (inst->nb_ast_params != 1 ||
-			(inst->ast_params[0].enum_token != TOKEN_DIRECT_LABEL &&
-			 inst->ast_params[0].enum_token != TOKEN_DIRECT_NUMBER))
-	{
-		ft_printf("lfork bad parameters\n");
-		return 1;
-	}
-	return 0;
-};
-
-int check_aff(t_ast_inst *inst)
-{
-	if (inst->nb_ast_params != 1 ||
-			inst->ast_params[0].enum_token != TOKEN_LABEL)
-	{
-		ft_printf("aff bad parameters\n");
-		return 1;
-	}
-	return 0;
+    if (inst->nb_ast_params != op_desc.nb_arg)
+        return 1;
+    i = 0;
+    while (i < inst->nb_ast_params)
+    {
+        if (check_param(inst->ast_params[i].enum_token, op_desc.args[i]))
+            return 1;
+    }
+    return 0;
 };
 
 int check_inst(t_ast_inst *inst)
 {
-	if (ft_strequ(inst->cmd, "live"))
-		return check_live(inst);
-	if (ft_strequ(inst->cmd, "ld"))
-		return check_ld(inst);
-	if (ft_strequ(inst->cmd, "st"))
-		return check_st(inst);
-	if (ft_strequ(inst->cmd, "add"))
-		return check_add(inst);
-	if (ft_strequ(inst->cmd, "sub"))
-		return check_sub(inst);
-	if (ft_strequ(inst->cmd, "and"))
-		return check_and(inst);
-	if (ft_strequ(inst->cmd, "or"))
-		return check_or(inst);
-	if (ft_strequ(inst->cmd, "xor"))
-		return check_xor(inst);
-	if (ft_strequ(inst->cmd, "zjmp"))
-		return check_zjmp(inst);
-	if (ft_strequ(inst->cmd, "ldi"))
-		return check_ldi(inst);
-	if (ft_strequ(inst->cmd, "sti"))
-		return check_sti(inst);
-	if (ft_strequ(inst->cmd, "fork"))
-		return check_fork(inst);
-	if (ft_strequ(inst->cmd, "lld"))
-		return check_lld(inst);
-	if (ft_strequ(inst->cmd, "lldi"))
-		return check_lldi(inst);
-	if (ft_strequ(inst->cmd, "lfork"))
-		return check_lfork(inst);
-	if (ft_strequ(inst->cmd, "aff"))
-		return check_aff(inst);
-	else
+	int i;
+
+	i = 0;
+
+	while (i < OP_TAB_LENGTH)
 	{
-		ft_printf("error: unknow cmd: %s", inst->cmd);
-		return 1;
+	    if (ft_strequ(gopt()[i].name, inst->cmd))
+            return check_cmd(inst, gopt()[i]);
+        i++;
 	}
+	ft_printf("error: unknow cmd: %s", inst->cmd);
+	return 1;
 };
 
 int check_insts(t_ast_inst *inst)
