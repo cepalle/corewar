b7c7f0ba669e6883564199a09600ab727506e822
cepalle <epalle.cyrille@gmail.com>
2018-04-24 17:13:54 +0200

    test update, need no handle multi line sring

--- test_asm/main.sh
+++ test_asm/main.sh
@@ -2,13 +2,14 @@
 
 files=`find champs -type f`;
 error=""
+msg_error=""
 
 for f in $files
 do
- echo "Processing $f"
+ #echo "Processing $f"
  ./bin_ref/asm $f > /dev/null
  mv `echo $f | sed "s/\.s/\.cor/g"` tmp1.cor
- ./asm $f > /dev/null
+ msg_error=`./asm $f`
  mv `echo $f | sed "s/\.s/\.cor/g"` tmp2.cor
  hexdump -C tmp1.cor > tmp1
  hexdump -C tmp2.cor > tmp2
@@ -18,9 +19,11 @@ do
  if [ "$error" != "" ]
  then
    echo "error with $f: \n$error\n";
+   echo "$msg_error";
    break
  fi
  error=""
+ msg_error=""
 done
 
 
