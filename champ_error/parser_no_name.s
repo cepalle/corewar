ed1da1bdd15622ecad0cae679a51d785639e3ea1
Epalle <cyrille.epalle@epita.fr>
2018-04-28 20:09:30 +0200

    add test, fixe seg fault

--- champ_error/parser_no_name.s
+++ champ_error/parser_no_name.s
@@ -1,5 +1,5 @@
-#.name           "42" #.name           "422"
-.comment        "Just a basic Winner Program"
+.name           "42" #.name           "422"
+#.comment        "Just a basic Winner Program"
 #.comment        "2Just a basic Winner Program"
 #%1
 
