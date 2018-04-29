67be30ae7f802b74d020876865f8287ec18323df
Audric Rolland <rolland.audric@gmail.com>
2018-04-29 18:37:13 +0200

    mise aux norme debug ok

--- srcs_corewar/set_up_vm.c
+++ srcs_corewar/set_up_vm.c
@@ -18,11 +18,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-int		ft_count_player(char ** argv)
+int				ft_count_player(char **argv)
 {
-	int a;
-	size_t len;
-	int count;
+	int		a;
+	size_t	len;
+	int		count;
 
 	ft_printf("ft_count_player\n");
 	a = 1;
@@ -30,7 +30,7 @@ int		ft_count_player(char ** argv)
 	while (argv[a])
 	{
 		len = ft_strlen(argv[a]);
-		if (len > 4 && ft_strncmp( argv[a] + (len - 4), ".cor", 4) == 0)
+		if (len > 4 && ft_strncmp(argv[a] + (len - 4), ".cor", 4) == 0)
 		{
 			ft_printf("%s est un champion\n", argv[a]);
 			count++;
@@ -42,60 +42,44 @@ int		ft_count_player(char ** argv)
 	return (count++);
 }
 
-
 static	void	ft_fill_player(char *argv, t_vm *vm, int num_player)
 {
-	int a;
-	int fd;
+	int		a;
+	int		fd;
 	ssize_t	ret;
 
 	ft_printf("ft_fill_player\n");
-
 	fd = open(argv, O_RDONLY);
 	if (fd == -1)
 		ft_printf("erreur ouverture fd\n");
-
 	ret = read(fd, &vm->player[num_player].head, sizeof(t_header));
-
+//	swap obligatoire pour reconstruire la bonne information
 	swap_4(&vm->player[num_player].head.magic);
 	swap_4(&vm->player[num_player].head.prog_size);
-
-	ft_printf("\n\n\n head.prog.size = %d\n",vm->player[num_player].head.prog_size );
-
 	if (ret == sizeof(t_header))
 		ft_printf("j'ai cree le header du joueur\n");
-
-	vm->player[num_player].prog = malloc(sizeof(char*) * vm->player[num_player].head.prog_size);
-
-	ret = read(fd, vm->player[num_player].prog, vm->player[num_player].head.prog_size);
-
-	a = 0;
-	while (a < vm->player[num_player].head.prog_size)
-	{
-		ft_printf("%0.2hhx ", vm->player[num_player].prog[a]);
-		a++;
-	}
-	ft_printf("\n");
-//	close(fd);
-
-
+	vm->player[num_player].prog = malloc(sizeof(char*) *
+								vm->player[num_player].head.prog_size);
+	ret = read(fd, vm->player[num_player].prog,
+		vm->player[num_player].head.prog_size);
+//	close(fd); double free si il est mis
 }
 
-void	ft_create_player(char **argv, t_vm *vm)
+void			ft_create_player(char **argv, t_vm *vm)
 {
-	int a;
-	int num_player;
-	size_t len;
+	int		a;
+	int		num_player;
+	size_t	len;
 
 	a = 1;
 	num_player = 0;
 	vm->nb_p = ft_count_player(argv);
-	if(!(vm->player = malloc(sizeof(t_player) * vm->nb_p)))
+	if (!(vm->player = malloc(sizeof(t_player) * vm->nb_p)))
 		return ;
 	while (argv[a])
 	{
 		len = ft_strlen(argv[a]);
-		if (len > 4 && ft_strncmp( argv[a] + (len - 4), ".cor", 4) == 0)
+		if (len > 4 && ft_strncmp(argv[a] + (len - 4), ".cor", 4) == 0)
 		{
 			ft_printf("je vais travailler sur le joueur %s\n", argv[a]);
 			ft_fill_player(argv[a], vm, num_player);
@@ -105,7 +89,7 @@ void	ft_create_player(char **argv, t_vm *vm)
 	}
 }
 
-void	ft_create_map(t_vm *vm)
+void			ft_create_map(t_vm *vm)
 {
 	int a;
 	int res;
@@ -118,7 +102,8 @@ void	ft_create_map(t_vm *vm)
 	ft_memset(vm->tab, 0, MEM_SIZE);
 	while (a < vm->nb_p)
 	{
-		ft_memcpy(vm->tab + res, vm->player[a].prog, vm->player[a].head.prog_size);
+		ft_memcpy(vm->tab + res, vm->player[a].prog,
+				vm->player[a].head.prog_size);
 		res = res + placement;
 		a++;
 	}
