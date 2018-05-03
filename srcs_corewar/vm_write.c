e28b9ea45d8385021b6777cc92d3ab0186c4273e
Audric Rolland <aurollan@z1r11p1.le-101.fr>
2018-05-03 13:17:42 +0200

    cette fois c'est bon

--- srcs_corewar/vm_write.c
+++ srcs_corewar/vm_write.c
@@ -9,25 +9,29 @@ void	vm_write_1(t_vm *vm, int PC, unsigned char data)
 void	vm_write_2(t_vm *vm,  int PC, unsigned short data)
 {
 	int a;
+	unsigned char *ptr;
 
 	a = 0;
-	swap_2(data);
+	ptr = (unsigned char*)&data;
+	swap_2(&data);
 	while (a < 2)
 	{
-		vm->tab[(PC + a) % MEM_SIZE] = (char)data[a];
+		vm->tab[(PC + a) % MEM_SIZE] = ptr[a];
 		a++;
 	}
 }
 
-void	vm_write_4(t_vm *vm, int PC, unsigned char data)
+void	vm_write_4(t_vm *vm, int PC, unsigned int data)
 {
 	int a;
+	unsigned char *ptr;
 
 	a = 0;
-	swap_4(data);
+	ptr = (unsigned char*)&data;
+	swap_4(&data);
 	while (a < 4)
 	{
-		vm->tab[(PC + a) % MEM_SIZE] = (char)data[a];
+		vm->tab[(PC + a) % MEM_SIZE] = ptr[a];
 		a++;
 	}
 }
