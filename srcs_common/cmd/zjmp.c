03868dede6e0dca4b0f2116a54ebab092627b5fa
cepalle <epalle.cyrille@gmail.com>
2018-05-04 12:04:19 +0200

    zjmp done

--- srcs_common/cmd/zjmp.c
+++ srcs_common/cmd/zjmp.c
@@ -1,16 +1,18 @@
 #include <corewar.h>
 
-// TODO carry
 int		cmd_zjmp(t_vm *vm, int ipr)
 {
-	int		er;
-	int		p1;
+	t_vm_proc	vm_proc;
+	int			p1;
 
-	er = 0;
-	(void)vm;
-	p1 = read_param(vm->process + ipr, 0, &er);
-	if (er)
+	init_vm_proc(&vm_proc, vm, ipr, 0);
+	p1 = read_param(&vm_proc, 0);
+	if (vm_proc.er || !vm->process[ipr].carry)
+	{
+		vm->process[ipr].PC = cal_PC_add(vm->process[ipr].PC,
+		vm->process[ipr].cmd_save.cmd_len);
 		return (0);
+	}
 	vm->process[ipr].PC = cal_PC_add(vm->process[ipr].PC, p1);
 	return (1);
 }
