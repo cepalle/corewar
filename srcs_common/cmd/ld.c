02cd069bf1df2a1050ba13cd27d3ac84f70694ac
Audric Rolland <aurollan@z1r11p1.le-101.fr>
2018-05-05 15:18:23 +0200

    debug intuile cor ok apres maj

--- srcs_common/cmd/ld.c
+++ srcs_common/cmd/ld.c
@@ -20,16 +20,19 @@ int		cmd_ld(t_vm *vm, int ipr)
 	int				ri1;
 
 	ft_printf("cmd_ld\n");
-
 	ft_printf("joueur %s\n", vm->player[ipr].head.prog_name);
-
 	ft_printf("\nipr = %d\n\n", ipr);
-
 	ft_printf("pc = %d\n", vm->process[ipr].PC);
+
 
 	init_vm_proc(&vm_proc, vm, ipr, 1);
 
+
+	ft_printf("num reg: %d\n", vm->process[ipr].cmd_save.params[0]);
+
 	ri1 = read_param(&vm_proc, 0);
+
+	ft_printf("ri1 = %d\n", ri1);
 
 	load_param(&vm_proc, 1, ri1);
 
