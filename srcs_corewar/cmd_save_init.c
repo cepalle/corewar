d9585ac81a0a9f135a5f9a7c715110ff90b06d4f
Pier'antonio Pichierri <pierantonio.pichierri@gmail.com>
2018-05-05 15:14:38 +0200

    modif pc PROCESSOR cmd_save_init.c

--- srcs_corewar/cmd_save_init.c
+++ srcs_corewar/cmd_save_init.c
@@ -15,7 +15,7 @@
 #include "libft.h"
 #include "op.h"
 
-/*static void 	ft_debug(t_proc *processor, int op)
+static void 	ft_debug(t_proc *processor, int op)
 {
 	int i;
 
@@ -23,27 +23,27 @@
 	ft_printf("ft_debug\n");
 	while (i < gopt()[op].nb_arg)
 	{
-		//ft_printf("params_type[%d] = %d    params_size[%d] = %d     "
-		//				  "params[%d] = %d   \n",i,
+		ft_printf("params_type[%d] = %d    params_size[%d] = %d     "
+						  "params[%d] = %d   \n",i,
 				  processor->cmd_save.params_type[i], i,
 				  processor->cmd_save.params_size[i],i ,
 				  processor->cmd_save.params[i]);
 		i++;
 	}
-}*/
+}
 
 static int 		ft_cmd_save_check_existence(int op, unsigned char tmp, int i)
 {
 	unsigned char a;
 
-	//ft_printf("ft_cmd_save_check_existence\n");
+	ft_printf("ft_cmd_save_check_existence\n");
 	a = (unsigned char)gopt()[op].args[i];
 	if (tmp == 3)
 		tmp = 4;
-	//ft_printf("a[%d] = %d  ", i, a);
+	ft_printf("a[%d] = %d  ", i, a);
 	if ((a & tmp) != 0)
 		return (1);
-	//ft_printf("params dans args[%d] non valide", i);
+	ft_printf("params dans args[%d] non valide", i);
 	return (0);
 }
 
@@ -54,7 +54,7 @@ static	int 	ft_cmd_save_right_params(t_vm *vm, t_proc *processor, int op)
 	int i;
 	int right;
 
-	//ft_printf("ft_cmd_save_right_params\n");
+	ft_printf("ft_cmd_save_right_params\n");
 	i = 0;
 	masque = 0b11000000;
 	right = 6;
@@ -67,7 +67,7 @@ static	int 	ft_cmd_save_right_params(t_vm *vm, t_proc *processor, int op)
 		right = right - 2;
 		if (ft_cmd_save_check_existence(op, tmp, i) == 0)
 			return (0);
-		//ft_printf("tmp_save_right_params[%d] = %d\n", i, tmp);
+		ft_printf("tmp_save_right_params[%d] = %d\n", i, tmp);
 		i++;
 	}
 	return (1);
@@ -78,13 +78,13 @@ static	int 	ft_cmd_save_error_oct_params(t_vm *vm, t_proc *processor)
 	unsigned char tmp;
 	int i;
 
-	//ft_printf("ft_cmd_save_error_oct_params\n");
+	ft_printf("ft_cmd_save_error_oct_params\n");
 	i = 0;
 	tmp = vm->tab[cal_pc_add(processor->PC, 1)]; // TODO remplacer par cal_PC_add ? // vm->tab[cal_pc_add(process->PC, 1)]
 	tmp = tmp << 6;
 	if (tmp != 0)
 	{
-		//ft_printf("erreur : 2 derniers bits non egal a zero = %d \n", tmp);
+		ft_printf("erreur : 2 derniers bits non egal a zero = %d \n", tmp);
 		return (0);
 	}
 	return (1);
@@ -93,16 +93,16 @@ static	int 	ft_cmd_save_error_oct_params(t_vm *vm, t_proc *processor)
 
 static int 	ft_no_oct_params(t_vm *vm, t_proc *processor, int op) // remplacement de vm_read_x(vm, (vm->process->PC + 1) % MEM_SIZE); par
 {
-	//ft_printf("ft_no_oct_params\n");
+	ft_printf("ft_no_oct_params\n");
 	if (gopt()[op].opcode == 1)
 	{
 		processor->cmd_save.params_size[0] = 4;
-		processor->cmd_save.params[0] = vm_read_4(vm, cal_pc_add(vm->process->PC, 1));
+		processor->cmd_save.params[0] = vm_read_4(vm, cal_pc_add(processor->PC, 1));
 	}
 	else
 	{
 		processor->cmd_save.params_size[0] = 2;
-		processor->cmd_save.params[0] = vm_read_2(vm, cal_pc_add(vm->process->PC, 1));
+		processor->cmd_save.params[0] = vm_read_2(vm, cal_pc_add(processor->PC, 1));
 	}
 	processor->cmd_save.params_type[0] = 2;
 	return (1);
@@ -125,7 +125,7 @@ static	int		ft_analyze_oct_params(t_vm *vm, t_proc *processor, int op) // rempla
 		return (0);
 	while (i < gopt()[op].nb_arg)
 	{
-		tmp = vm->tab[(processor->PC + 1) % MEM_SIZE]; // TODO remplacer par cal_PC_add ?
+		tmp = vm->tab[cal_pc_add(processor->PC, 1)]; // TODO remplacer par cal_PC_add ?
 		tmp = tmp << left;
 		tmp = tmp >> 6;
 		if (tmp == REG_CODE)
@@ -133,7 +133,7 @@ static	int		ft_analyze_oct_params(t_vm *vm, t_proc *processor, int op) // rempla
 			processor->cmd_save.params_type[i] = 1;
 			processor->cmd_save.params_size[i] = 1;
 			processor->cmd_save.params[i] = vm_read_1(vm,
-											cal_pc_add(vm->process->PC, cpt)); //TODO a checker si modulo bien formatte
+											cal_pc_add(processor->PC, cpt)); //TODO a checker si modulo bien formatte
 			cpt = cpt + 1;
 		}
 		else if (tmp == DIR_CODE && gopt()[op].dir_size_2 == 0)
@@ -141,7 +141,7 @@ static	int		ft_analyze_oct_params(t_vm *vm, t_proc *processor, int op) // rempla
 			processor->cmd_save.params_type[i] = 2;
 			processor->cmd_save.params_size[i] = 4;
 			processor->cmd_save.params[i] = vm_read_4(vm,
-											cal_pc_add(vm->process->PC, cpt));
+											cal_pc_add(processor->PC, cpt));
 			cpt = cpt + 4;
 		}
 		else if (tmp == DIR_CODE && gopt()[op].dir_size_2 == 1)
@@ -149,14 +149,14 @@ static	int		ft_analyze_oct_params(t_vm *vm, t_proc *processor, int op) // rempla
 			processor->cmd_save.params_type[i] = 2;
 			processor->cmd_save.params_size[i] = 2;
 			processor->cmd_save.params[i] = vm_read_2(vm,
-											cal_pc_add(vm->process->PC, cpt));
+											cal_pc_add(processor->PC, cpt));
 			cpt = cpt + 2;
 		}
 		else if (tmp == IND_CODE)
 		{
 			processor->cmd_save.params_type[i] = 3;
 			processor->cmd_save.params_size[i] = 2; 							//TODO a checker si size = 2 ou 4 !!
-			ind_value = vm_read_2(vm, cal_pc_add(vm->process->PC, cpt));
+			ind_value = vm_read_2(vm, cal_pc_add(processor->PC, cpt));
 			processor->cmd_save.params[i] = (signed short)ind_value;
 			cpt = cpt + 2;
 		}
@@ -164,7 +164,7 @@ static	int		ft_analyze_oct_params(t_vm *vm, t_proc *processor, int op) // rempla
 		i++;
 	}
 	processor->cmd_save.cmd_len = cpt;
-	//ft_printf("len = %d\n", processor->cmd_save.cmd_len);
+	ft_printf("len = %d\n", processor->cmd_save.cmd_len);
 	return (1);
 }
 
@@ -192,15 +192,15 @@ int			stock_cmd(t_vm *vm, t_proc *processor)
 		return (0);
 	if (gopt()[op].octet_param == 1)
 	{
-		//ft_printf("op vaut %d\n", gopt()[op].opcode);   // DEBUG
-		//ft_printf("op correspond a %s\n", gopt()[op].name); //DEBUG
+		ft_printf("op vaut %d\n", gopt()[op].opcode);   // DEBUG
+		ft_printf("op correspond a %s\n", gopt()[op].name); //DEBUG
 		ft_analyze_oct_params(vm, processor, op);
 	}
 	else
 		ft_no_oct_params(vm, processor, op);
 	processor->cmd_save.cycle_wating = (unsigned int)gopt()[op].cycle;
 	processor->cmd_save.cmd = gopt()[op].op_fct;
-	//ft_printf("cycle %d\n", processor->cmd_save.cycle_wating); //DEBUG
-	//ft_debug(processor, op);
+	ft_printf("cycle %d\n", processor->cmd_save.cycle_wating); //DEBUG
+	ft_debug(processor, op);
 	return (1);
 }
