c6e748fb6f874cd360d6d2185bef18e8fe99aa1f
Audric Rolland <aurollan@z1r11p1.le-101.fr>
2018-05-10 14:27:53 +0200

    affichage des contestant au debut du combat pas a la fin

--- srcs_corewar/vm_run.c
+++ srcs_corewar/vm_run.c
@@ -12,11 +12,22 @@
 /* ************************************************************************** */
 
 #include "corewar.h"
+#include "libft.h" // a supp
 
 void	vm_run(t_vm *vm)
 {
 	int		nb_no_decr;
+	int		a;
 
+	a = 0;
+	ft_printf("Introducing contestants...\n");
+	while (a < vm->nb_p)
+	{
+		ft_printf("* Player %d, weighing %d bytes, '%s' ('%s') !\n", a + 1,
+				  vm->player[a].head.prog_size, vm->player[a].head.prog_name,
+				  vm->player[a].head.comment);
+		a++;
+	}
 	vm->cycle = 0;
 	vm->cycle_last_check = vm->cycle;
 	vm->cycle_to_die = CYCLE_TO_DIE;
