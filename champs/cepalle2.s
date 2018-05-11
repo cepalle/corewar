618238b050c9c2ba92377bc5e7e3f4eec18e8839
cepalle <epalle.cyrille@gmail.com>
2018-05-11 10:45:38 +0200

    champ add

--- champs/cepalle2.s
+++ champs/cepalle2.s
@@ -1,12 +1,8 @@
-.name "Explosive Kitty"
-.comment "Jan-ken-pon (じゃんけんぽん)
-Ce qui reste éternellement incompréhensible dans la nature, c'est qu'on puisse la comprendre.
-Celui qui ne peut plus éprouver ni étonnement ni surprise, est pour ainsi dire mort : ses yeux sont éteints. – Albert Einstein
-Si vous voulez trouver les secrets de l'univers pensez en termes de fréquences, d'énergies et de vibrations. - Nikola Tesla
-Pas la pour etre ici. - Silicon Val[list]
-Dieu est la plus belle invention.
-Tapis dans l'ombre je veille. - Minou"
+.name "fsfad"
+.comment "Ha."
 
-ld %0, r1
-ldi %512, %30, r1
-zjmp %0
+    sti  r1, %:l1, %1
+l1: live %1
+    fork %:l1
+    ld %57672193, r2
+    st r2, 56
